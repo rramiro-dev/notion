e35b1a858967447785baadaa59543e107325f0d2
rramiro-dev <reche.ramiro98@gmail.com>
2024-03-24 01:14:57 -0300

    Agregue las variables de entorno para los scripts de la integracion con las bases de datos de finanzas personales

---
+++ finanzas/environment_variables.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+# Recorre directorios hacia arriba hasta encontrar el archivo .env
+dotenv_path = find_dotenv()
+
+# Carga el contenido de .env como variables de entorno
+load_dotenv(dotenv_path)
+
+API_KEY = os.getenv("API_KEY")
