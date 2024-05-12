01f0c58c675a4b2f82e87ddbbfe3ced42d44960c
rramiro-dev <reche.ramiro98@gmail.com>
2024-05-11 22:37:43 -0300

    Started all over again. Env variables created and set up.

---
+++ expenses/main.py
@@ -0,0 +1,11 @@
+from dotenv import find_dotenv, load_dotenv
+import os
+
+dotenv_path = find_dotenv() # Recorre directorios hacia arriba hasta encontrar el archivo .env
+load_dotenv(dotenv_path) # Carga el contenido de .env como variables de entorno
+
+NOTION_SECRET_API_KEY = os.getenv("NOTION_SECRET_API_KEY")
+NOTION_DATABASE_ID_CATEGORIAS = os.getenv("NOTION_DATABASE_ID_CATEGORIAS")
+NOTION_DATABASE_ID_INGRESOS = os.getenv("NOTION_DATABASE_ID_INGRESOS")
+# GOOGLE_SPREADSHEET_ID = os.getenv("")
+
