8be1f05315bbe6a1122d7097970ab09359dac36c
rramiro-dev <reche.ramiro98@gmail.com>
2024-05-12 02:31:25 -0300

    I was able to get all Categorias database data using the Notion API, and organized it in different json files. I have also created a new python file to separate the code that searches and gather the environment variables values

--- expenses/main.py
+++ expenses/main.py
@@ -1,10 +1,70 @@
-from dotenv import find_dotenv, load_dotenv
-import os
+from notion_client import Client
+import json
+import environment_variables as env
+from time import asctime, localtime
 
-dotenv_path = find_dotenv() # Recorre directorios hacia arriba hasta encontrar el archivo .env
-load_dotenv(dotenv_path) # Carga el contenido de .env como variables de entorno
+def write_dict_to_file_as_json(content, file_name, extension='.json'):
+    '''
+        creates a json file with the given content, named as the file_name's value
+    '''
+    content_as_json_str = json.dumps(content)
 
-NOTION_SECRET_API_KEY = os.getenv("NOTION_SECRET_API_KEY")
-NOTION_DATABASE_ID_CATEGORIAS = os.getenv("NOTION_DATABASE_ID_CATEGORIAS")
-NOTION_DATABASE_ID_INGRESOS = os.getenv("NOTION_DATABASE_ID_INGRESOS")
-GOOGLE_SPREADSHEET_ID = os.getenv("GOOGLE_SPREADSHEET_ID")
+    if not file_name.endswith(extension):
+        file_name += extension
+
+    with open(file_name, 'w') as f:
+        f.write(content_as_json_str)
+
+def safe_get(data, dot_chained_keys):
+    '''
+        {'a': {'b': [{'c': 1}]}}
+        safe_get(data, 'a.b.0.c') -> 1
+
+        Secure way to access to the values within the given argument 'data'
+    '''
+    keys = dot_chained_keys.split('.')
+    for key in keys:
+        try:
+            if isinstance(data, list):
+                data = data[int(key)]
+            else:
+                data = data[key]
+        except (KeyError, TypeError, IndexError):
+            return None
+    return data
+
+def main():
+    # Authentication
+    client = Client(auth=env.NOTION_SECRET_API_KEY)
+
+    # Creates a database json file
+    db_info = client.databases.retrieve(database_id=env.NOTION_DATABASE_ID_CATEGORIAS)
+    write_dict_to_file_as_json(db_info, 'db_info.json')
+
+    # Creates a database's rows json file
+    db_rows = client.databases.query(database_id=env.NOTION_DATABASE_ID_CATEGORIAS)
+    write_dict_to_file_as_json(db_rows, 'db_rows.json')
+
+    # Access to database's rows data``
+    simplified_rows = []
+
+    for row in db_rows['results']:
+        id = safe_get(row, 'properties.ID.unique_id.number')
+        name = safe_get(row, 'properties.Name.title.0.plain_text')
+        mes_actual = safe_get(row, 'properties.$ Mes actual.formula.number')
+        mes_anterior = safe_get(row, 'properties.$ Mes anterior.formula.number')
+        presupuesto = safe_get(row, 'properties.Presupuesto.number')
+
+        simplified_rows.append({
+            'id': id,
+            'name': name,
+            'mes_actual': mes_actual,
+            'mes_anterior': mes_anterior,
+            'presupuesto': presupuesto,
+            'last_updated_time': asctime(localtime())
+        })
+
+    write_dict_to_file_as_json(simplified_rows, 'simplified_rows.json')
+
+if __name__ == '__main__':
+    main()
